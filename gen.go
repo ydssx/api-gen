018f25b916bf711103a8854d00c90464dfdf488d
wangxin <wangxin@91cyt.com>
2023-05-20 19:32:26 +0800

    update

--- gen.go
+++ gen.go
@@ -3,89 +3,35 @@ package main
 import (
 	"fmt"
 	"go/ast"
-	"io"
+	"go/format"
+	"go/parser"
+	"go/printer"
+	"go/token"
+	"log"
 	"os"
 	"strings"
-
-	"github.com/pkg/errors"
 )
 
-type ApiInfo struct {
-	Path        string
-	Method      string
-	HandlerName string
-}
-
-func ParseComments(comment string) (info ApiInfo) {
-	list := strings.Fields(comment)
-	for i := 0; i < len(list); i++ {
-		switch list[i] {
-		case "@handler":
-			info.HandlerName = list[i+1]
-		case "@router":
-			info.Path = list[i+1]
-			info.Method = strings.ToUpper(list[i+2])
-		}
-	}
-	return
-}
-
-type ApiStruct struct {
-	Req     string
-	Resp    string
-	PkgName string
-}
-
-func parseStructs(pkgName string, structNames []string) (r ApiStruct) {
-	for _, v := range structNames {
-		if suffix := strings.TrimSuffix(v, "Req"); suffix != v {
-			r.Req = pkgName + "." + v
-		} else if suffix := strings.TrimSuffix(v, "Resp"); suffix != v {
-			r.Resp = pkgName + "." + v
-		}
-	}
-	return
-}
-
-type LogicInfo struct {
-	Pkg      string
-	FuncName string
-	Receiver string
-	Results  []string
-}
-
-func parseLogic(pkg string, dec *ast.FuncDecl) (l LogicInfo) {
-	l.Pkg = pkg
-	l.FuncName = dec.Name.Name
-	resultList := dec.Type.Results.List
-	result := []string{}
-	for _, v := range resultList {
-		result = append(result, v.Names[0].Name)
-	}
-	l.Results = result
-	return
-}
-
 var logicTmp = `
-func LoginLogic(req %s) (resp %s, err error) {
+func %sLogic(req %s) (resp %s, err error) {
 	// TODO: add your logic here and delete this line
 
 	return
 }
 `
 
-func genLogic(filename string, api ApiStruct) {
-	content := fmt.Sprintf(logicTmp, api.Req, api.Resp)
-	fileAppend(filename, content)
+func genLogicFunc(filename string, api TypeInfo) FuncInfo {
+	content := fmt.Sprintf(logicTmp, api.HandlerName, api.Req, api.Resp)
+	return writeDecl(filename, content)
 }
 
 var handlerTmp = `
-// @Success 200	{object} model.Admin
-// @Router /accounts/{id} [get]
+// @Success 200	{object} util.Response{data=%s}
+// @Router %s [%s]
 func %sHandler(c *gin.Context) {
 	var req %s
 	if err := c.ShouldBind(&req); err != nil {
-		util.FailWithMsg(c, util.WrapErrMsg(err))
+		util.FailWithMsg(c, util.WrapValidateErrMsg(err))
 		return
 	}
 
@@ -99,28 +45,130 @@ func %sHandler(c *gin.Context) {
 }
 `
 
-func genHandler(filename string, def ApiStruct, api ApiInfo, logic LogicInfo) {
+func genHandlerFunc(filename string, def TypeInfo, logic FuncInfo) FuncInfo {
 
 	// 要追加的内容
-	content := fmt.Sprintf(handlerTmp, api.HandlerName, def.Req, strings.Join(logic.Results, ", "), logic.Pkg, logic.FuncName, logic.Results[0])
+	content := fmt.Sprintf(handlerTmp, def.Resp, def.Path, strings.ToLower(def.Method), def.HandlerName, def.Req, strings.Join(logic.Results, ", "), logic.Pkg, logic.FuncName, logic.Results[0])
 
-	fileAppend(filename, content)
-
+	return writeDecl(filename, content)
 }
 
-func fileAppend(filename, content string) error {
-	// 打开文件，如果文件不存在则创建，以追加模式打开
-	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+func writeDecl(filename, decl string) (info FuncInfo) {
+	// 解析文件
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
 	if err != nil {
-		return errors.Wrap(err, "Failed to open file")
-	}
-	defer file.Close()
-
-	// 将内容写入文件
-	if _, err := io.WriteString(file, content); err != nil {
-		return errors.Wrap(err, "Failed to write to file")
+		log.Fatal(err)
 	}
 
-	fmt.Println("Content appended to file.")
+	// 将新函数的源代码解析为语法树
+	funcAST, err := parser.ParseFile(fset, "", "package main\n\n"+decl, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err)
+	}
+	newFunc := funcAST.Decls[0].(*ast.FuncDecl)
+	info = parseFunc(file.Name.Name, newFunc)
+
+	// 将新函数添加到原始文件的语法树中
+	file.Decls = append(file.Decls, newFunc)
+
+	output, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer output.Close()
+
+	// 格式化生成的代码
+	err = format.Node(output, fset, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("New function added to", filename)
+	return
+}
+
+func addRouter(routerFile, routerFunc string, apiInfo TypeInfo, handlerFunc FuncInfo) (err error) {
+
+	// 解析Go文件
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, routerFile, nil, parser.ParseComments)
+	if err != nil {
+		fmt.Println("Failed to parse file:", err)
+		return
+	}
+
+	// 查找目标函数
+	var targetFunc *ast.FuncDecl
+	for _, decl := range file.Decls {
+		if fn, ok := decl.(*ast.FuncDecl); ok && fn.Name.Name == routerFunc {
+			targetFunc = fn
+			break
+		}
+	}
+	if targetFunc == nil {
+		return fmt.Errorf("Failed to find target func :%s ,%v", routerFunc, err)
+	}
+
+	// 创建新的CallExpr节点
+	newCallExpr := &ast.ExprStmt{
+		X: &ast.CallExpr{
+			Fun: &ast.SelectorExpr{
+				X:   ast.NewIdent(targetFunc.Type.Params.List[0].Names[0].Name),
+				Sel: ast.NewIdent(apiInfo.Method),
+			},
+			Args: []ast.Expr{
+				ast.NewIdent(`"` + apiInfo.Path + `"`),
+				ast.NewIdent(handlerFunc.Pkg + "." + handlerFunc.FuncName),
+			},
+		},
+	}
+	funcBody := targetFunc.Body
+	// 在目标函数体的语句列表中找到适当的位置插入新的调用表达式
+	insertIndex := findInsertIndex(funcBody.List, targetFunc.Pos(), targetFunc.End())
+	funcBody.List = append(funcBody.List[:insertIndex], append([]ast.Stmt{newCallExpr}, funcBody.List[insertIndex:]...)...) // 创建新的路由语句
+
+	outputFile, err := os.OpenFile(routerFile, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println("Failed to create file:", err)
+		return
+	}
+	defer outputFile.Close()
+
+	err = formatAndWriteFile(outputFile, fset, file)
+	if err != nil {
+		fmt.Println("Failed to write file:", err)
+		return
+	}
+
+	fmt.Println("New statement added to", routerFile)
 	return nil
+}
+
+func formatAndWriteFile(outputFile *os.File, fset *token.FileSet, file *ast.File) error {
+	// 创建缓冲区来保存格式化后的代码
+	var buf strings.Builder
+
+	// 使用格式化配置进行代码格式化
+	err := printer.Fprint(&buf, fset, file)
+	if err != nil {
+		return err
+	}
+
+	// 格式化后的代码写入文件
+	_, err = outputFile.WriteString(buf.String())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func findInsertIndex(stmts []ast.Stmt, startPos, endPos token.Pos) int {
+	for i, stmt := range stmts {
+		if stmt.Pos() >= endPos {
+			return i
+		}
+	}
+	return len(stmts)
 }
---
+++ parse.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"strings"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+type ApiInfo struct {
+	Path        string
+	Method      string
+	HandlerName string
+}
+
+func ParseComments(comment string) (info ApiInfo) {
+	list := strings.Fields(comment)
+	for i := 0; i < len(list); i++ {
+		switch list[i] {
+		case "@handler":
+			info.HandlerName = cases.Title(language.English).String(list[i+1])
+		case "@router":
+			info.Path = list[i+1]
+			info.Method = strings.Trim(strings.ToUpper(list[i+2]), "[]")
+		}
+	}
+	return
+}
+
+type TypeInfo struct {
+	Req     string
+	Resp    string
+	PkgName string
+	ApiInfo
+}
+
+func parseStructs(pkgName string, structNames []string) (r TypeInfo) {
+	for _, v := range structNames {
+		if suffix := strings.TrimSuffix(v, "Req"); suffix != v {
+			r.Req = pkgName + "." + v
+		} else if suffix := strings.TrimSuffix(v, "Resp"); suffix != v {
+			r.Resp = pkgName + "." + v
+		}
+	}
+	r.PkgName = pkgName
+	return
+}
+
+type FuncInfo struct {
+	Pkg      string
+	FuncName string
+	Results  []string
+}
+
+func parseFunc(pkg string, dec *ast.FuncDecl) (l FuncInfo) {
+	l.Pkg = pkg
+	l.FuncName = dec.Name.Name
+	results := dec.Type.Results
+	result := []string{}
+	if results != nil {
+		for _, v := range results.List {
+			result = append(result, v.Names[0].Name)
+		}
+	}
+	l.Results = result
+	return
+}
+
+func parseCodeTmp(code string) (*ast.FuncDecl, error) {
+	code = "package main\n\n" + code
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "", code, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+	if len(file.Decls) == 0 {
+		panic("no statements")
+	}
+	fn, ok := file.Decls[0].(*ast.FuncDecl)
+	if !ok {
+		panic("not a function")
+	}
+	// if len(fn.Body.List) == 0 {
+	// 	panic("no statements")
+	// }
+	return fn, nil
+}
+
+func parseTypes(filename, path string) (info TypeInfo) {
+	var apiInfo ApiInfo
+	var types []string
+
+	fset := token.NewFileSet()
+
+	astFile, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, decl := range astFile.Decls {
+		if v, ok := decl.(*ast.GenDecl); ok {
+			for _, spec := range v.Specs {
+				if typeSpec, ok := spec.(*ast.TypeSpec); ok {
+					if _, ok := typeSpec.Type.(*ast.StructType); ok {
+						if v.Doc != nil {
+							// structName := typeSpec.Name.Name
+							commentText := v.Doc.Text()
+							info := ParseComments(commentText)
+							if info.Path == path {
+								apiInfo = info
+								types = append(types, typeSpec.Name.Name)
+							}
+						}
+					}
+				}
+			}
+
+		}
+	}
+	info = parseStructs(astFile.Name.Name, types)
+	info.ApiInfo = apiInfo
+	return
+}
