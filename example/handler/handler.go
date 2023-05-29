fdd7c1b5f51fe46c98cd90131ed83c0397884fce
ydssx <wangxin@91cyt.com>
2023-05-29 14:04:22 +0800

    update

--- example/handler/handler.go
+++ example/handler/handler.go
@@ -8,28 +8,10 @@ import (
 )
 
 // @Security ApiKeyAuth
-// @Param Login body types.LoginReq true "请求参数"
-// @Success 200	{object} util.Response{data=types.LoginResp}
-// @Router api1/login [post]
-func LoginHandler(c *gin.Context) {
-	var req types.LoginReq
-	if err := c.ShouldBind(&req); err != nil {
-		util.FailWithMsg(c, util.WrapValidateErrMsg(err))
-		return
-	}
-
-	resp, err := logic.LoginLogic(req)
-	if err != nil {
-		util.FailWithMsg(c, err.Error())
-		return
-	}
-
-	util.OKWithData(c, resp)
-}
-
 // @Param Register query types.RegisterReq true "请求参数"
 // @Success 200	{object} util.Response{data=types.RegisterResp}
-// @Router apiv2/register [get]
+// @Failure 400	{object} util.Response
+// @Router /user/register [get]
 func RegisterHandler(c *gin.Context) {
 	var req types.RegisterReq
 	if err := c.ShouldBind(&req); err != nil {
@@ -38,6 +20,28 @@ func RegisterHandler(c *gin.Context) {
 	}
 
 	resp, err := logic.RegisterLogic(req)
+	if err != nil {
+		util.FailWithMsg(c, err.Error())
+		return
+	}
+
+	util.OKWithData(c, resp)
+}
+
+// @Summary 用户登录
+// @Security ApiKeyAuth
+// @Param Login body types.LoginReq true "请求参数"
+// @Success 200	{object} util.Response{data=types.LoginResp}
+// @Failure 400	{object} util.Response
+// @Router /login [post]
+func LoginHandler(c *gin.Context) {
+	var req types.LoginReq
+	if err := c.ShouldBind(&req); err != nil {
+		util.FailWithMsg(c, util.WrapValidateErrMsg(err))
+		return
+	}
+
+	resp, err := logic.LoginLogic(req)
 	if err != nil {
 		util.FailWithMsg(c, err.Error())
 		return
